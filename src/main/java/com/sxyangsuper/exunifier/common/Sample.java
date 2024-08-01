21b828a360a480f65f99b732adb9a5f0caae7cc0
Shixiong Yang <shixiong.yang@rea-group.com>
2024-08-01 21:05:26 +0800

    introduce PMD,spotbugs,log in

--- src/main/java/com/sxyangsuper/exunifier/common/Sample.java
+++ src/main/java/com/sxyangsuper/exunifier/common/Sample.java
@@ -1,7 +1,13 @@
 package com.sxyangsuper.exunifier.common;
 
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor
 public class Sample {
     public String helloWorld() {
+        log.debug("this is helloWorld method");
         return "Hello world";
     }
 }
