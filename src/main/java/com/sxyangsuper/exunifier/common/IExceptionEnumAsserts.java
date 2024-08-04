52afa838cff0154882203101923c8a85ef5fbaf9
Shixiong Yang <shixiong.yang@rea-group.com>
2024-08-04 23:01:18 +0800

    add union interface IExceptionEnumAsserts

---
+++ src/main/java/com/sxyangsuper/exunifier/common/IExceptionEnumAsserts.java
@@ -0,0 +1,6 @@
+package com.sxyangsuper.exunifier.common;
+
+import com.sxyangsuper.exunifier.common.asserts.IAsserts;
+
+public interface IExceptionEnumAsserts<E extends BaseException> extends IExceptionEnum, IAsserts<E> {
+}
