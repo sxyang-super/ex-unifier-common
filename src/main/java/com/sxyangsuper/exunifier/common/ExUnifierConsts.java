475deda9953f9773dace9ac453093eeb3001496f
Shixiong Yang <shixiong.yang@rea-group.com>
2024-08-05 21:06:32 +0800

    add consts

---
+++ src/main/java/com/sxyangsuper/exunifier/common/ExUnifierConsts.java
@@ -0,0 +1,9 @@
+package com.sxyangsuper.exunifier.common;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ExUnifierConsts {
+    public static final String EXCEPTION_SOURCE_QUALIFIED_NAME = "com.sxyangsuper.exunifier.common.ExceptionSource";
+}
--- src/main/java/com/sxyangsuper/exunifier/common/ExceptionSource.java
+++ src/main/java/com/sxyangsuper/exunifier/common/ExceptionSource.java
@@ -6,6 +6,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * If this class qualified name changed,
+ * need to update {@link ExUnifierConsts#EXCEPTION_SOURCE_QUALIFIED_NAME}.
+ */
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({ ElementType.TYPE })
